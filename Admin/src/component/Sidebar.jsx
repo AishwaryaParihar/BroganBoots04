bb41b4c766e093cf8d9699aa5ed54a249bf7e069
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-10-05 12:28:23 +0530

    side bar

---
+++ Admin/src/component/Sidebar.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+// import { Link, Outlet } from 'react-router-dom';
+// import { RiAdminFill } from 'react-icons/ri';
+
+const Sidebar = () => {
+  return (
+    // <div className="min-h-screen md:flex">
+    //   {/* Sidebar */}
+    //   <aside className="bg-primary min-h-full w-60 text-white">
+    //     {/* Admin Icon and Title */}
+    //     <div className="h-32 my-12 flex flex-col justify-center items-center">
+    //       <div className="text-8xl cursor-pointer">
+    //         <RiAdminFill />
+    //       </div>
+    //       <p className="capitalize text-lg font-semibold">Admin</p>
+    //     </div>
+
+    //     {/* Navigation Links */}
+    //     <nav className="p-4 space-y-4">
+    //       <Link
+    //         to="dashboard"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         Dashboard
+    //       </Link>
+    //       <Link
+    //         to="contactForm"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         Contact Details
+    //       </Link>
+    //       <Link
+    //         to="register"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         Registration
+    //       </Link>
+    //       <Link
+    //         to="samples"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         Donor Register
+    //       </Link>
+    //       <Link
+    //         to="volunteer"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         Volunteer Register
+    //       </Link>
+    //       <Link
+    //         to="userRecordForm"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         User Record Form
+    //       </Link>
+    //       <Link
+    //         to="userRecord"
+    //         className="block px-2 py-1 hover:bg-slate-100 hover:text-black font-semibold transition"
+    //       >
+    //         User Record
+    //       </Link>
+    //     </nav>
+    //   </aside>
+
+    //   {/* Main Content */}
+    //   <main className="flex-1 p-6 overflow-auto">
+    //     <Outlet />
+    //   </main>
+    // </div>
+    <div className="">grt</div>
+  );
+};
+
+export default Sidebar;
