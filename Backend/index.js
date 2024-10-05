a0e77aedd4130df9ca1e0251a778429147e07df8
testuser <testuser@gmail.com>
2024-10-05 14:49:03 +0530

    index

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,15 @@ const express = require('express')
 const app = express('')
 const connectDB = require('./config/db');
 const router = require('./router/router');
+const cors = require("cors");
+const FRONTEND_URL= process.env.FRONTEND_URL
 
-
+app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      credentials: true,
+    })
+  );
 app.get ('/',(req,res)=>{
     res.status(200).send('this is root')
 })
