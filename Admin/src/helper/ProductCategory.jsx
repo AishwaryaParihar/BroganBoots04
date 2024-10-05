1386d380fb107f546b4a71e097fdcffbc3f5fa5f
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-10-05 16:10:24 +0530

    product upload ui

---
+++ Admin/src/helper/ProductCategory.jsx
@@ -0,0 +1,9 @@
+const ProductCategory = [
+    {id : "1", label : "Rugged", value : "rugged"},
+    {id : "2", label : "Suede", value : "suede"},
+    {id : "3", label : "Chelsea", value : "chelsea"},
+    {id : "4", label : "Lace-up", value : "laceup"},
+    {id : "5", label : "Stomper", value : "stomper"},
+
+]
+export default ProductCategory
