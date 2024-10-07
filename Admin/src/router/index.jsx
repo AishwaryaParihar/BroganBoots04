816e0abae80bda80fe084effaffaaa005eac19d4
Karishma <karishmadawar123@gmail.com>
2024-10-07 18:18:22 +0530

    kk

--- Admin/src/router/index.jsx
+++ Admin/src/router/index.jsx
@@ -3,11 +3,8 @@ import App from "../App";
 import Product from "../component/content/Product";
 import Sidebar from "../component/Sidebar";
 import ContactList from "../component/content/ContactList";
-<<<<<<< HEAD
 import Login from "../component/Login";
-=======
-import AboutUpdate from "../component/content/AboutUpdate";
->>>>>>> b7bbf9f9d14f28b850eba70f16f7cd0cfaa1062b
+
 
 const router = createBrowserRouter([
     {
@@ -28,15 +25,13 @@ const router = createBrowserRouter([
                     },
                     {
                         path: "contact",
-<<<<<<< HEAD
                         element: <ContactList />  // Ensure this component exists
-=======
-                        element: < ContactList/>  
+
                     },
                     {
                         path: "aboutUpdate",
                         element: < AboutUpdate/>  
->>>>>>> b7bbf9f9d14f28b850eba70f16f7cd0cfaa1062b
+
                     }
                 ]
             }
